d2c148f434f7d4412294fff5067dba7c274168f2
Dimitri Brukakis <dbrukakis@cellular.de>
2020-11-05 17:04:31 +0100

    Fix build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,13 +10,13 @@ xcodebuild -showsdks;
 swift package generate-xcodeproj;
 
 # Build Framework in Debug and Run Tests if specified
-if [ $RUN_TESTS == "NO" ]; then
+if [ "$RUN_TESTS" == "NO" ]; then
     xcodebuild -project RemoteConfiguration.xcodeproj -scheme RemoteConfiguration-Package -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO | xcpretty;
 else
     xcodebuild test -project RemoteConfiguration.xcodeproj -scheme RemoteConfiguration-Package -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES | xcpretty;
 fi
 # Run `pod lib lint` if specified
-if [ $POD_LINT == "YES" ]; then
+if [ "$POD_LINT" == "YES" ]; then
 	gem install cocoapods --no-document --quiet;
     pod lib lint;
 fi
